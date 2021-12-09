823eca42675c4a894dc72525d5b69efad9e70cd1
Hansen <zhangheshan@yiruikecorp.com>
2021-12-09 17:45:22 +0800

    [GL][添加脚本]

--- SwiftyFileBrowser.podspec
+++ SwiftyFileBrowser.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
     s.name             = 'SwiftyFileBrowser'
     s.version          = '0.1.0'
-    s.summary          = 'A short description of SwiftyFileBrowser.'
+    s.summary          = 'A file browser lib of swift(icon type comming soon!).'
     
     # This description is used to generate tags and improve search results.
     #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,13 +18,13 @@ Pod::Spec.new do |s|
     #   * Finally, don't worry about the indent, CocoaPods strips it!
     
     s.description      = <<-DESC
-    TODO: Add long description of the pod here.
+    A file browser lib of swift.
     DESC
     
     s.homepage         = 'https://github.com/Hansen/SwiftyFileBrowser'
-    # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+    s.screenshots      = 'https://raw.githubusercontent.com/ghostlordstar/SwiftyFileBrowser/main/ScreenShot/st_list.png'
     s.license          = { :type => 'MIT', :file => 'LICENSE' }
-    s.author           = { 'Hansen' => 'zhangheshan@yiruikecorp.com' }
+    s.author           = { 'Hansen' => 'heshanzhang@outlook.com' }
     s.source           = { :git => 'https://github.com/Hansen/SwiftyFileBrowser.git', :tag => s.version.to_s }
     # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
     
---
+++ publish_pod.py
@@ -0,0 +1,137 @@
+import os
+import sys
+
+def selectOperator():
+
+    print("")
+    print("* 1. 直接push到公共仓库")
+    print("* 2. 提交到develop分支")
+    print("* 3. 提交到main分支")
+    print("* 4. 提交到main并push到公共仓库")
+    print("")
+    return
+
+
+def isRepoDirty():
+    return os.system("git diff --quiet") != 0
+
+
+def switchBranch(branch):
+
+    branchType = int(branch)
+
+    if branchType in [1, 2, 3] == False:
+        print("")
+        print("* 1. 停留在当前分支")
+        print("* 2. 切换到develop分支")
+        print("* 3. 切换到main分支")
+        print("")
+
+        # 输入切换的选项
+        branchType = int(input("请选择要切换到的分支:"))
+    else:
+        print("切换到的类型为：%d", branchType)
+
+    # 切换到指定分支
+    if branchType == 2:
+        os.system("git checkout develop")
+    elif branchType == 3:
+        os.system("git checkout main")
+    else:
+        print("停留在当前分支。")
+    return
+
+
+def pushToGit(branchName):
+
+    pushBranch = branchName
+    if len(branchName) <= 0:
+        pushBranch = "main"
+    else:
+        pushBranch = branchName
+
+    print("开始发布到当前分支 ...")
+    os.system("git status")
+    os.system("git add --all")
+    msg = input("\n请输入提交信息(默认为'日常提交'):")
+    print("提交信息：%s" % msg)
+    os.system("git commit -m '%s'" % ("日常提交" if msg.isspace() else msg))
+    os.system("git pull origin %s" % pushBranch)
+    os.system("git push origin %s" % pushBranch)
+    print("已经上传到%s成功了" % pushBranch)
+    return
+
+
+def addGitTag(tag):
+
+    os.system("git tag %s" % tag)
+    os.system("git push origin --tags")
+    return
+
+
+def pushToMain():
+    switchBranch(3)
+    pushToGit("main")
+    return
+
+# 提交到main，并且打tag，推送到cocopods
+def pushToMainAndPushSpecs():
+    switchBranch(3)
+    pushToGit("main")
+
+    tag = str(input("输入要发布的版本号(跳过设置 Tag 请直接回车)："))
+    if len(tag) <= 0:
+        print("输入版本号为空，跳过设置 Tag 步骤")
+    else:
+        addGitTag(tag)
+    pushToPrivateSpecs()
+
+
+# 直接push到CTSpecs
+def pushToPrivateSpecs():
+
+    checkCommand = "pod spec lint %s.podspec https://github.com/CocoaPods/Specs' --allow-warnings"%podName
+    pushCommand = "pod repo push %s.podspec https://github.com/CocoaPods/Specs' --allow-warnings --use-modular-headers"%podName
+    updatePirvateRepo = "pod repo update"
+    checkRet = os.system(checkCommand)
+    if checkRet != 0:
+        print("校验出错，请检查spec文件是否配置正确")
+        exit - 1
+    else:
+        pushRet = os.system(pushCommand)
+        if pushRet != 0:
+            print("上传失败！")
+            exit - 1
+        else:
+            print("上传完成！！")
+            os.system(updatePirvateRepo)
+    return
+
+
+if __name__ == "__main__":
+    # 进入到发布路径
+    #os.chdir(sys.argv[1])
+    # 输入库名称
+    # podName = str(input("请输入要发布的pod库名称:"))
+    podName = "SwiftyFileBrowser"
+    # 输入操作类型
+    selectOperator()
+    # 输入发布类型
+    option = input("请选择发布的类型(默认为1):")
+    # option 不输入时默认为1，否则为输入内容
+    if len(option) <= 0:
+        option = 1
+    else:
+        option = int(option)
+
+    # 去发布
+    if option == 1:
+        pushToPrivateSpecs()
+    elif option == 2:
+        pushToGit("develop")
+    elif option == 3:
+        pushToGit("main")
+    elif option == 4:
+        pushToMainAndPushSpecs()
+    else:
+        print("🤨 -> 请输入正确的选项!!")
